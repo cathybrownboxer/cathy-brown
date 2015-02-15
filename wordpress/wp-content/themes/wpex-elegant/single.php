22d2ae91a1ff655a9e1d56a89ec294a507c79db6
ttbarnes <tony@tonybarnes.me>
2015-02-15 13:05:07 +0000

    blog post page formatting/layout, sass

--- wordpress/wp-content/themes/wpex-elegant/single.php
+++ wordpress/wp-content/themes/wpex-elegant/single.php
@@ -9,36 +9,57 @@
 
 get_header(); ?>
 
+<div class="social-icons">
+	<ul>
+		<li class="facebook"><a href="#" target="_blank">
+			<i class="fa fa-facebook-square fa-lg"></i>
+		</a></li>
+		<li class="twitter"><a href="#" target="_blank">
+			<i class="fa fa-twitter-square fa-lg"></i>
+		</a></li>
+		<li class="linkedin"><a href="#" target="_blank">
+			<i class="fa fa-linkedin-square fa-lg"></i>
+		</a></li>
+		<li class="youtube"><a href="#" target="_blank">
+			<i class="fa fa-youtube-square fa-lg"></i>
+		</a></li>
+	</ul>
+</div>
+
 <?php while ( have_posts() ) : the_post(); ?>
 	<div id="primary" class="content-area clr">
-		<div id="content" class="site-content left-content clr" role="main">
+		<div id="content" class="site-content" role="main">
 			<article>
-				<?php
-				if ( !post_password_required() ) {
-					get_template_part( 'content', get_post_format() );
-				} ?>
-				<header class="page-header clr">
-					<h1 class="page-header-title"><?php the_title(); ?></h1>
-					<?php
-					// Display post meta
-					// See functions/commons.php
-					wpex_post_meta(); ?>
-				</header><!-- .page-header -->
-				<div class="entry clr">
-					<?php the_content(); ?>
-				</div><!-- .entry -->
-				<footer class="entry-footer">
-					<?php edit_post_link( __( 'Edit Post', 'wpex' ), '<span class="edit-link clr">', '</span>' ); ?>
-				</footer><!-- .entry-footer -->
+				<div class="page-content" class="entry clr">
+					<div class="left-container">
+						<h1 class="page-header-title"><?php the_title(); ?></h1>
+
+						<?php
+						if ( !post_password_required() ) {
+							get_template_part( 'content', get_post_format() );
+						} ?>
+
+						<?php the_content(); ?>
+
+						<footer class="entry-footer">
+							<?php edit_post_link( __( 'Edit Post', 'wpex' ), '<span class="edit-link clr">', '</span>' ); ?>
+						</footer><!-- .entry-footer -->
+						<?php
+							// Display author bio
+							// See functions/commons.php
+							wpex_post_author(); ?>
+							<?php comments_template(); ?>
+
+					</div>
+
+					<div class="right-container">
+						<?php get_sidebar(); ?>
+					</div>
+
+				</div>
 			</article>
-			<?php
-			// Display author bio
-			// See functions/commons.php
-			wpex_post_author(); ?>
-			<?php comments_template(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links clr">', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 		</div><!-- #content -->
-		<?php get_sidebar(); ?>
 	</div><!-- #primary -->
 <?php endwhile; ?>
 <?php get_footer(); ?>
