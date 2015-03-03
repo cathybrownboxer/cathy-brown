e4bebc42508a6f99d8f896ca2208bc83c5fc150e
ttbarnes <tony@tonybarnes.me>
2015-03-03 19:33:40 +0000

    footer

--- wordpress/wp-content/themes/wpex-elegant/footer.php
+++ wordpress/wp-content/themes/wpex-elegant/footer.php
@@ -15,32 +15,34 @@
 	<div id="footer-wrap" class="footer-wrap clr">
 		<div id="footer" class="clr footer-container">
 			<div id="footer-widgets" class="clr">
-				<div class="footer-box span_1_of_3 col col-1">
+				<div class="footer-box span_1_of_3 col col-5">
 					<?php dynamic_sidebar( 'footer-one' ); ?>
 				</div><!-- .footer-box -->
 				<div class="footer-box span_1_of_3 col col-2">
 					<?php dynamic_sidebar( 'footer-two' ); ?>
 				</div><!-- .footer-box -->
-				<div class="footer-box span_1_of_3 col col-3">
+				<div class="footer-box span_1_of_3 col col-3 social-icons">
 					<!--<?php dynamic_sidebar( 'footer-three' ); ?>-->
-					<div class="social-icons">
-						<ul>
-							<li class="facebook"><a href="https://www.facebook.com/cathybrownboxer" target="_blank">
-								<i class="fa fa-facebook-square fa-lg"></i>
-							</a></li>
-							<li class="twitter"><a href="https://twitter.com/cathybrownboxer" target="_blank">
-								<i class="fa fa-twitter-square fa-lg"></i>
-							</a></li>
-							<li class="linkedin"><a href="https://uk.linkedin.com/pub/cathy-brown/41/793/798" target="_blank">
-								<i class="fa fa-linkedin-square fa-lg"></i>
-							</a></li>
-							<li class="youtube"><a href="https://www.youtube.com/user/thecathybrown" target="_blank">
-								<i class="fa fa-youtube-square fa-lg"></i>
-							</a></li>
-						</ul>
-					</div>
+					<ul>
+						<li class="facebook"><a href="https://www.facebook.com/cathybrownboxer" target="_blank">
+							<i class="fa fa-facebook-square fa-lg"></i>
+						</a></li>
+						<li class="twitter"><a href="https://twitter.com/cathybrownboxer" target="_blank">
+							<i class="fa fa-twitter-square fa-lg"></i>
+						</a></li>
+						<li class="linkedin"><a href="https://uk.linkedin.com/pub/cathy-brown/41/793/798" target="_blank">
+							<i class="fa fa-linkedin-square fa-lg"></i>
+						</a></li>
+						<li class="youtube"><a href="https://www.youtube.com/user/thecathybrown" target="_blank">
+							<i class="fa fa-youtube-square fa-lg"></i>
+						</a></li>
+					</ul>
 				</div><!-- .footer-box -->
 			</div><!-- #footer-widgets -->
+
+			<div class="row footer-copyright">
+				<p><?php wpex_copyright(); ?></p>
+			</div>
 		</div><!-- #footer -->
 	</div><!-- #footer-wrap -->
 
